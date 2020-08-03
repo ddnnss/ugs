c10e1f7540bb6c150b254d49b4138797b2be99ef
ddnnss <ddnnss.i1@gmail.com>
2020-08-03 09:58:06 +0300

    add new payments and social auth

--- ugs/settings.py
+++ ugs/settings.py
@@ -29,22 +29,75 @@ SOCIAL_AUTH_MAILRU_OAUTH2_SECRET = settings.MAIL_SECRET
 
 SOCIAL_AUTH_FACEBOOK_KEY = settings.FACEBOOK_KEY
 SOCIAL_AUTH_FACEBOOK_SECRET = settings.FACEBOOK_SECRET
-SOCIAL_AUTH_FACEBOOK_API_VERSION = '2.8'
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link'] # add this
-SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {       # add this
-  'fields': 'id, name, email, picture.type(large), link'
+SOCIAL_AUTH_FACEBOOK_API_VERSION = '2.4'
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id,name,email',
 }
-SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = [                 # add this
-    ('name', 'name'),
-    ('email', 'email'),
-    ('picture', 'picture'),
-    ('link', 'profile_url'),
-]
+
+SOCIAL_AUTH_ODNOKLASSNIKI_OAUTH2_KEY = settings.OK_ID
+SOCIAL_AUTH_ODNOKLASSNIKI_OAUTH2_SECRET = settings.OK_SECRET
+SOCIAL_AUTH_ODNOKLASSNIKI_OAUTH2_PUBLIC_NAME = 'Ugscash'
+
+SOCIAL_AUTH_ODNOKLASSNIKI_APP_EXTRA_USER_DATA_LIST =  {
+    'fields': 'id,name,email',
+}
+
+
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
 
+SOCIAL_AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social_core.pipeline.social_auth.social_details',
+
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social_core.pipeline.social_auth.social_uid',
+
+    # Verifies that the current auth process is valid within the current
+    # project, this is where emails and domains whitelists are applied (if
+    # defined).
+    'social_core.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social_core.pipeline.social_auth.social_user',
+
+    # # If there already is an account with the given steamid, pass it on to the pipeline
+    # 'authentication.pipeline.associate_existing_user',
+    #
+    # # The username for the account is always the steamid
+    # # 'social_core.pipeline.user.get_username', # Function to get the username was changed
+    # 'authentication.pipeline.get_username',
+
+    # Send a validation email to the user to verify its email address.
+    # Disabled by default.
+    # 'social_core.pipeline.mail.mail_validation',
+
+    # Associates the current social details with another user account with
+    # a similar email address. Disabled by default.
+    # 'social_core.pipeline.social_auth.associate_by_email',
+
+    # Create a user account if we haven't found one yet.
+    'social_core.pipeline.user.create_user',
+
+    # Create the record that associates the social account with the user.
+    'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social_core.pipeline.social_auth.load_extra_data',
+
+    # # Update the user record with any changed info from the auth service.
+    # # 'social_core.pipeline.user.user_details',
+    # # Use a custom function for this, since the details are provided separately
+    'social_core.pipeline.user.user_details',
+)
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
@@ -72,6 +125,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.vk.VKOAuth2',
     'social_core.backends.mailru.MailruOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.odnoklassniki.OdnoklassnikiOAuth2',
     'django.contrib.auth.backends.ModelBackend'
 )
 
