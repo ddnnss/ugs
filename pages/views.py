04efca92b6a6a2f8a266aba92bf06db18c3b8f0d
ddnnss <ddnnss.i1@gmail.com>
2020-08-04 19:47:46 +0300

    html fixes

--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,7 @@ from customuser.forms import NewMessageForm
 
 
 def index(request):
+    nonLK = True
     allFaqFirst = Faq.objects.filter(is_first=True)
     allFaqSecond = Faq.objects.filter(is_first=False)
     pageTitle = 'Кэшбэк ставки на спорт | Cash Back с проигрышей | Кэшбэк со ставок '
@@ -12,18 +13,21 @@ def index(request):
     return render(request, 'pages/index.html', locals())
 
 def about(request):
+    nonLK = True
     pageTitle = 'О системе UGS - кэшбэк сервис кто мы и чем сможем помочь'
     pageDescription = 'Подробная информация о сервисе, который создан для игроков, которые любят и будут рисковать 💥 UGS - финансовая подушка для игроков, которые привыкли играть на крупные суммы.'
     allFeedbacks = Feedback.objects.all()
     return render(request, 'pages/about.html', locals())
 
 def feedbacks(request):
+    nonLK = True
     pageTitle = 'Отзывы о системе UGS наших постоянных клиентов'
     pageDescription = 'UGS - финансовая подушка для игроков, которые привыкли играть на крупные суммы.'
     allFeedbacks = Feedback.objects.all()
     return render(request, 'pages/feedbacks.html', locals())
 
 def contacts(request):
+    nonLK = True
     pageTitle = 'UGS | Контакты | КЭШБЭК СЕРВИС В СТАВКАХ НА СПОРТ'
     pageDescription = 'Сервис создан для игроков, которые любят и будут рисковать. UGS - финансовая подушка для игроков, которые привыкли играть на крупные суммы'
     allFaqFirst = Faq.objects.filter(is_first=True)
